7f510a433c497470a9526b8d24219258f4db0a45
Justine <justinefokou29@gmail.com>
2026-01-18 22:23:54 +0100

    Fixr

--- quincaillerie/settings.py
+++ quincaillerie/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default="django-insecure-2(6b!ou)efk6*-bz3p7hr50(fw44sc%)@&z^rf+_u**n)#2cb7")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # Configuration des hôtes autorisés pour la production
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
